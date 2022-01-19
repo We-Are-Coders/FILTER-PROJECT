a3a00b2c165b7fa42a0e89072437116d2c103cb0
We-Are-Coders <92448003+We-Are-Coders@users.noreply.github.com>
2022-01-18 19:54:54 -0500

    Add files via upload

---
+++ Filter Project (Lipstick version)/main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  lipstick_image = loadImage('https://i.postimg.cc/J7bCZLNh/lipstick.jpg');
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw(){
+  image(video, 0 , 0, 300, 300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(noseX, noseY, 20);
+  image(lipstick_image, noseX, noseY, 30, 30)
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
